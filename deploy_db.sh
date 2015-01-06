87a1e7c98648c4b4c8bd8ee6d31327b2aaf2e4b6
Fabien Hermenier <fabien.hermenier@unice.fr>
2015-01-06 08:26:49 +0100

    clean

--- deploy_db.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#Dumping
-pg_dump -O wints > dump.sql
-heroku pg:psql < drop.sql
-heroku pg:psql < dump.sql
--- make_dist.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-GOPATH=`pwd` go install github.com/fhermeni/wints
-mkdir wints
-cp bin/wints wints/
-cp -r src/github.com/fhermeni/wints/static src/github.com/fhermeni/wints/wints.conf  wints/
-tar cfz wints.tar.gz wints
-rm -rf wints
