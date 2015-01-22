1e526a3c08f5e6e2baa7710dd43e3691033bb553
Fabien Hermenier <fabien.hermenier@unice.fr>
2015-01-22 22:22:47 +0100

    fix REST call

--- static2/js/rest.js
+++ static2/js/rest.js
@@ -106,7 +106,7 @@ function users(ok, no) {
 }
 
 function newUser(fn, ln, tel, email, role, ok, no) {
-    return post(ROOT_API + "/users/",{Firstname: fn, Lastname:ln, Tel: tel, Role: role,Email: email}, ok, no);    
+    return post("/users/",{Firstname: fn, Lastname:ln, Tel: tel, Role: role,Email: email}, ok, no);    
 }
 
 function rmUser(email, ok, no) {
