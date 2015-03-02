aaf067dc33608f0f2eb7f64eec12c9598c97baf2
Fabien Hermenier <fabien.hermenier@unice.fr>
2015-03-02 10:20:51 +0100

    fix deadline UI

--- static2/js/student.js
+++ static2/js/student.js
@@ -20,7 +20,7 @@ function showDashboard() {
         for (x in i.Reports) {            
             i.Reports[x].Uploaded = i.Reports[x].Grade != -2;
             //Locked= deadline passed || commented            
-            i.Reports[x].Locked = new Date(i.Reports[x].Deadline).getTime() < new Date().getTime() || i.Reports[x].Grade >= 0;                   
+            i.Reports[x].Locked = new Date(i.Reports[x].Deadline).getTime() > new Date().getTime() || i.Reports[x].Grade >= 0;                   
         }            
         var html = Handlebars.getTemplate("internship")(i);
         var root = $("#cnt");
