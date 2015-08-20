451de7356ba8704b12da10566a5dff80868de2b7
Fabien Hermenier <fabien.hermenier@unice.fr>
2015-08-20 23:57:15 +0200

    godoc

--- datastore/txerr.go
+++ datastore/txerr.go
@@ -2,16 +2,21 @@ package datastore
 
 import "database/sql"
 
+//TxErr is a structure to manage transactions more confortablely.
+//It maintains the ongoing error to state if the transaction must be committed or rollbacked
 type TxErr struct {
 	tx  *sql.Tx
 	err error
 }
 
+//newTxErr begins a new transaction
 func newTxErr(db *sql.DB) TxErr {
 	tx, err := db.Begin()
 	return TxErr{tx: tx, err: err}
 }
 
+//Done terminates the transaction.
+//It is committed if err == nil. Rollbacked otherwise.
 func (r *TxErr) Done() error {
 	if r.err != nil {
 		return r.tx.Rollback()
@@ -19,6 +24,7 @@ func (r *TxErr) Done() error {
 	return r.tx.Commit()
 }
 
+//Exec executes the query and store the resulting error variable
 func (r *TxErr) Exec(query string, args ...interface{}) {
 	if r.err != nil {
 		return
@@ -29,6 +35,8 @@ func (r *TxErr) Exec(query string, args ...interface{}) {
 	}
 }
 
+//Exec executes the update query, store the resulting error variable and returns
+//the number of updated rows
 func (r *TxErr) Update(query string, args ...interface{}) int64 {
 	if r.err != nil {
 		return -1
