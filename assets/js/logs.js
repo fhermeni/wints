473aa2b7da7281203dc795af834b56cd386741d9
Fabien Hermenier <fabien.hermenier@unice.fr>
2016-01-24 21:18:33 +0100

    scroller

--- assets/js/logs.js
+++ assets/js/logs.js
@@ -3,13 +3,37 @@
 function showLogs() {
 	logs().success(function(list) {
 		$("#cnt").render("logs", list, function() {
-			showLog();
+			$("a[href='#top']").click(function() {
+  				$("html, body").animate({ scrollTop: 0 }, 500);
+  				return false;
+			});
+			$("a[href='#bottom']").click(function() {
+  				$("html, body").animate({ scrollTop: $(document).height()-$(window).height()}, 500);
+  				return false;
+			});
+	
+			$('.pull-down').each(function() {
+    			$(this).css('margin-top', $(this).parent().height()-$(this).height())
+			});
+			events = list.filter(function (i) {
+				return i.indexOf("event") == 0;
+			});
+			toShow = events[0];
+			events.forEach(function (v,i) {
+				if (v > toShow) {					
+					toShow = v;
+				} 
+			});
+			$("#logs").val(toShow);
+			showLog(toShow);
 		})
 	});
 }
 
-function showLog() {
-	var f = $("#logs").val();
+function showLog(f) {
+	if (!f) {
+		f = $("#logs").val();
+	}	
 	getLog(f).success(function (l) {
 		$("#log").html(l);
 	})
