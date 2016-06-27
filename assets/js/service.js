b0a16b80cd6ec89c6d7d3d1d715a8308613106a3
Fabien Hermenier <fabien.hermenier@unice.fr>
2016-06-27 09:27:06 -0700

    report the number of defense sessions

--- assets/js/service.js
+++ assets/js/service.js
@@ -1,27 +1,38 @@
 function showService() {
 	showWait();
-	internships().done(function(ints) {
-		if (!ints) {
-			ints = [];
+	$.when(internships(), defenses())
+		.done(computeService);
+
+}
+
+function computeService(ints, ss) {
+	ints = ints[0];
+	ss = ss[0];
+
+	var service = {};
+	ints.forEach(function(i) {
+		var em = i.Convention.Tutor.Person.Email;
+		var p = i.Convention.Student.Promotion;
+		if (!service[em]) {
+			service[em] = {
+				U: i.Convention.Tutor,
+				Ints: {},
+				Defs: 0,
+				TotalInts: 0,
+			};
 		}
-		var service = {};
-		ints.forEach(function(i) {
-			var em = i.Convention.Tutor.Person.Email;
-			var p = i.Convention.Student.Promotion;
-			if (!service[em]) {
-				service[em] = {
-					U: i.Convention.Tutor,
-					Ints: {},
-					Defs: [],
-					TotalInts: 0
-				};
-			}
-			if (!service[em].Ints[p]) {
-				service[em].Ints[p] = []
-			}
-			service[em].Ints[p].push(i)
-			service[em].TotalInts++
-		});
-		$("#cnt").render("service", service, ui);
+		if (!service[em].Ints[p]) {
+			service[em].Ints[p] = []
+		}
+		service[em].Ints[p].push(i)
+		service[em].TotalInts++
 	});
+
+	ss.forEach(function (s) {
+		s.Juries.forEach(function(j) {
+			var em = j.Person.Email;
+			service[em].Defs++;
+		});
+	})
+	$("#cnt").render("service", service, ui);
 }
