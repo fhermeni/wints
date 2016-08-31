1c32ec46d89d831e248cf1684516b17d9879f557
Fabien Hermenier <fabien.hermenier@unice.fr>
2016-08-31 15:04:06 +0200

    fix defense service ?

--- assets/js/service.js
+++ assets/js/service.js
@@ -5,21 +5,26 @@ function showService() {
 
 }
 
+function newService(tutor) {
+	return {
+				U: tutor,
+				Ints: {},
+				Defs: 0,
+				TotalInts: 0
+	};
+}
 function computeService(ints, ss) {
 	ints = ints[0];
 	ss = ss[0];
 
+	console.log(ints);
+	console.log(ss);
 	var service = {};
 	ints.forEach(function(i) {
 		var em = i.Convention.Tutor.Person.Email;
 		var p = i.Convention.Student.Promotion;
 		if (!service[em]) {
-			service[em] = {
-				U: i.Convention.Tutor,
-				Ints: {},
-				Defs: 0,
-				TotalInts: 0,
-			};
+			service[em]  = newService(i.Convention.Tutor);
 		}
 		if (!service[em].Ints[p]) {
 			service[em].Ints[p] = []
@@ -31,6 +36,9 @@ function computeService(ints, ss) {
 	ss.forEach(function (s) {
 		s.Juries.forEach(function(j) {
 			var em = j.Person.Email;
+			if (!service[em]) {
+				service[em]  = newService(j);
+			}
 			service[em].Defs++;
 		});
 	})
